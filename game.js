de24ccb7f8eebb607a3499d7f07911e66f36dff0
piercecardin <72952209+piercecardin@users.noreply.github.com>
2020-10-16 00:36:44 -0600

    Update game.js

--- game.js
+++ game.js
@@ -19,7 +19,7 @@ charImage.onload = setAssetReady;
 charImage.src = PATH_CHAR;
 
 function setAssetReady() {
-  this.ready = true;
+    this.ready = true;
 }
 
 //Display Preloading in canvas (ie loading screen)
@@ -31,67 +31,75 @@ var preloader = setInterval(preloading, TIME_PER_FRAME);
 var gameloop, facing, sX, sY, charX, charY, isMoving, isRunning,
     mHp, hp, mSp, sp,
     runCost, runReplenish,
-    keyPressed, keyReleased;
+    keyPressed, keyReleased,
+    moveKeys;
 
 function preloading() {
-  if (charImage.ready) {
-    clearInterval(preloader);
+    if (charImage.ready) {
+        clearInterval(preloader);
 
-    //Initialise game
-    facing = "S"; //N = North, E = East, S = South, W = West
-    isMoving = false;
-    isRunning = false;
-    mHp = 100;
-    mSp = 100;
-    hp = mHp;
-    sp = mSp;
-    runCost = 0.5;
-    runReplenish = 1.0;
+        //Initialise game
+        facing = "S"; //N = North, E = East, S = South, W = West
+        isMoving = false;
+        isRunning = false;
+        mHp = 100;
+        mSp = 100;
+        hp = mHp;
+        sp = mSp;
+        runCost = 0.5;
+        runReplenish = 1.0;
+        moveKeys = ["W", "A", "S", "D"];
 
-    gameloop = setInterval(update, TIME_PER_FRAME);
-    document.addEventListener("keydown", keyDownHandler, false);
-    document.addEventListener("keyup", keyUpHandler, false);
-  }
+        gameloop = setInterval(update, TIME_PER_FRAME);
+        document.addEventListener("keydown", keyDownHandler, false);
+        document.addEventListener("keyup", keyUpHandler, false);
+    }
 }
 
 //------------
 //Key Handlers
 //------------
 function keyDownHandler(event) {
-  keyPressed = String.fromCharCode(event.keyCode);
-  
-  switch (keyPressed) {
-    case "W":
-      facing = "N";
-      isMoving = true;
-      break;
-    case "D":
-      facing = "E";
-      isMoving = true;
-      break;
-    case "S":
-      facing = "S";
-      isMoving = true;
-      break;
-    case "A":
-      facing = "W";
-      isMoving = true;
-      break;
-    case "SHIFT":
-      isRunning = true;
-      break;
-    default:
-      facing = "S";
-      isMoving = false;
-      //isRunning = false;
-      break;
-  }
+    keyPressed = String.fromCharCode(event.keyCode);
+    if (keyPressed == moveKeys) {
+        isMoving = true;
+    }
+    for (key = 0; key < 4; key++) {
+        if (key == "W") {
+            facing = "N";
+        } else if (key == "D") {
+            facing = "E";
+        } else if (key == "S") {
+            facing = "S";
+        } else if (key == "A") {
+            facing = "W";
+        }
+
+    }
+    /* Depracated
+    //This is meant only for facing vars.
+    switch (keyPressed) {
+        case "W":
+            facing = "N";
+            break;
+        case "D":
+            facing = "E";
+            break;
+        case "S":
+            facing = "S";
+            break;
+        case "A":
+            facing = "W";
+            break;
+        default:
+            facing = "S";
+            isMoving = false;
+            //isRunning = false;
+            break;
+    }
+    */
 }
 
-// This is a (bad) way of stopping the player. It causes a stutter, and must be fixed
-// But it is fine for now.
-// Maybe add an else to the keyhandler switch, so when they arent pressed, it is instant,
-// And not a seperate check.
 //Depracated
 /*
 function keyUpHandler(event) {
@@ -109,19 +117,19 @@ function keyUpHandler(event) {
 
 // New
 function keyUpHandler(event) {
-  keyReleased = String.fromCharCode(event.keyCode);
+    keyReleased = String.fromCharCode(event.keyCode);
 
-  if ((keyReleased == "W") || (keyReleased == "A") ||
-      (keyReleased == "S") || (keyReleased == "D")) {
-    if ((keyPressed != "W") ||
-        (keyPressed != "A") ||
-        (keyPressed != "S") ||
-        (keyPressed != "D")) {
-      isMoving = false;
-    }else{
-        //nothing
+    if ((keyReleased == "W") || (keyReleased == "A") ||
+        (keyReleased == "S") || (keyReleased == "D")) {
+        if ((keyPressed != "W") ||
+            (keyPressed != "A") ||
+            (keyPressed != "S") ||
+            (keyPressed != "D")) {
+            isMoving = false;
+        } else {
+            //nothing
+        }
     }
-  }
 }
 
 //------------
@@ -134,75 +142,75 @@ sX = IMAGE_START_X;
 sY = IMAGE_START_EAST_Y;
 
 function update() {
-  //Clear Canvas
-  ctx.fillStyle = "grey";
-  ctx.fillRect(0, 0, stage.width, stage.height);
+    //Clear Canvas
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0, 0, stage.width, stage.height);
 
-  // Animation handler
-  if (isMoving) {
+    // Animation handler
+    if (isMoving) {
 
-    // Sets the animation marker for image based on player
-    // Direction, and also moves the player based on this.
-    // This also handles running
-    //todo Un-constantify speed
-    switch (facing) {
-      case "N":
-        if ((isRunning) && (sp > 0)) {
-          charY -= CHAR_RUNSPEED;
-          sp -= runCost;
-        } else {
-          charY -= CHAR_SPEED;
-          //sp += runReplenish;
+        // Sets the animation marker for image based on player
+        // Direction, and also moves the player based on this.
+        // This also handles running
+        //todo Un-constantify speed
+        switch (facing) {
+            case "N":
+                if ((isRunning) && (sp > 0)) {
+                    charY -= CHAR_RUNSPEED;
+                    sp -= runCost;
+                } else {
+                    charY -= CHAR_SPEED;
+                    //sp += runReplenish;
+                }
+                sY = IMAGE_START_NORTH_Y;
+                break;
+
+            case "E":
+                if ((isRunning) && (sp > 0)) {
+                    charX += CHAR_RUNSPEED;
+                    sp -= runCost;
+                } else {
+                    charX += CHAR_SPEED;
+                    //sp += runReplenish;
+                }
+                sY = IMAGE_START_EAST_Y;
+                break;
+
+            case "S":
+
+                if ((isRunning) && (sp > 0)) {
+                    charY += CHAR_RUNSPEED;
+                    sp -= runCost;
+                } else {
+                    charY += CHAR_SPEED;
+                    //sp += runReplenish;
+                }
+                sY = IMAGE_START_SOUTH_Y;
+                break;
+
+            case "W":
+
+                if ((isRunning) && (sp > 0)) {
+                    charX -= CHAR_RUNSPEED;
+                    sp -= runCost;
+                } else {
+                    charX -= CHAR_SPEED;
+                    //sp += runReplenish;
+                }
+                sY = IMAGE_START_WEST_Y;
+                break;
         }
-        sY = IMAGE_START_NORTH_Y;
-        break;
 
-      case "E":
-        if ((isRunning) && (sp > 0)) {
-          charX += CHAR_RUNSPEED;
-          sp -= runCost;
-        } else {
-          charX += CHAR_SPEED;
-          //sp += runReplenish;
+        // Actual animation handler
+        sX += CHAR_WIDTH;
+
+        if (sX >= SHEET_WIDTH) {
+            sX = 0;
         }
-        sY = IMAGE_START_EAST_Y;
-        break;
-
-      case "S":
-
-        if ((isRunning) && (sp > 0)) {
-          charY += CHAR_RUNSPEED;
-          sp -= runCost;
-        } else {
-          charY += CHAR_SPEED;
-          //sp += runReplenish;
-        }
-        sY = IMAGE_START_SOUTH_Y;
-        break;
-
-      case "W":
-
-        if ((isRunning) && (sp > 0)) {
-          charX -= CHAR_RUNSPEED;
-          sp -= runCost;
-        } else {
-          charX -= CHAR_SPEED;
-          //sp += runReplenish;
-        }
-        sY = IMAGE_START_WEST_Y;
-        break;
+    } else {
+        sX = 0;
     }
-
-    // Actual animation handler
-    sX += CHAR_WIDTH;
-
-    if (sX >= SHEET_WIDTH) {
-      sX = 0;
-    }
-  } else {
-    sX = 0;
-  }
-  //Draw Image
-  ctx.drawImage(charImage, sX, sY, CHAR_WIDTH, CHAR_HEIGHT,
-                charX, charY, CHAR_WIDTH, CHAR_HEIGHT);
+    //Draw Image
+    ctx.drawImage(charImage, sX, sY, CHAR_WIDTH, CHAR_HEIGHT,
+        charX, charY, CHAR_WIDTH, CHAR_HEIGHT);
 }
